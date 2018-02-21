e0edf2a52f802538554305add4db5188575fd7ff
家娃 <guanjing.pangj@alibaba-inc.com>
2018-02-21 20:35:48 +0800

    support download the shufa picture.

--- web.py
+++ web.py
@@ -1,5 +1,27 @@
 import urllib.request
+from bs4 import BeautifulSoup
 
-response = urllib.request.urlopen('http://python.org')
-contents = response.read()
-print(contents)
+
+def main():
+    site = 'http://www.yac8.com/'
+    url = site + "news/list_136.html"
+    respone = urllib.request.urlopen(url)
+    contents = respone.read()
+    soup = BeautifulSoup(contents, 'html5lib')
+    dict = {}
+    for link in soup.find_all('img'):
+        title = link.get('title')
+        if title != '置顶' and title != None:
+            dict[title] = link.get("src")
+
+    for x in dict:
+        v = dict.get(x)
+        r = urllib.request.urlopen(site + v[3:])
+        contents = r.read()
+        f = open(x + ".jpg", 'wb')
+        f.write(contents)
+        f.close()
+
+
+if __name__ == '__main__':
+    main()
