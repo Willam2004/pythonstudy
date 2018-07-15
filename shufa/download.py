7dc1313db531aa7345a3948c5a6efc31040be11e
家娃 <guanjing.pangj@alibaba-inc.com>
2018-07-15 17:55:02 +0800

    add

--- shufa/download.py
+++ shufa/download.py
@@ -2,14 +2,14 @@ from urllib import request
 from requests_html import HTMLSession
 import os
 def download(args):
-    v = "/Users/jiawa/PycharmProjects/pythonstudy/shufa/images/"
+    v = ""
     u = request.urlopen(args)
     cat_img = u.read()
     filename = os.path.basename(args)
     with open(v + filename, 'wb') as f:
         f.write(cat_img)
 
-        
+
 session = HTMLSession()
 url="https://www.sohu.com/a/195049321_717378"
 r = session.get(url)
