a05fba13b3fa769c8d585d3def1d8765006f44b6
家娃 <guanjing.pangj@alibaba-inc.com>
2018-07-15 17:54:01 +0800

    add shufa

---
+++ shufa/download.py
@@ -0,0 +1,20 @@
+from urllib import request
+from requests_html import HTMLSession
+import os
+def download(args):
+    v = "/Users/jiawa/PycharmProjects/pythonstudy/shufa/images/"
+    u = request.urlopen(args)
+    cat_img = u.read()
+    filename = os.path.basename(args)
+    with open(v + filename, 'wb') as f:
+        f.write(cat_img)
+
+        
+session = HTMLSession()
+url="https://www.sohu.com/a/195049321_717378"
+r = session.get(url)
+imgs = r.html.find('img')
+for item in imgs:
+        s = item.attrs['src']
+        download(s)
+
